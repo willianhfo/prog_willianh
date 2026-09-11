e4753e3c81e1fa42230eacdcba1ebec64e66ae09
willianhfo <faria.oliveira.willian@escola.pr.gov.br>
2026-09-11 15:20:18 +0000

    aula01

--- aula01/exercicios.py
+++ aula01/exercicios.py
@@ -6,30 +6,60 @@ Escreva sua solucao no lugar do 'pass'.
 
 
 def soma_lista(lista):
-    """Devolve a soma de todos os numeros da lista. Lista vazia devolve 0."""
-    pass
-
+    soma = 0
+    for i in lista:
+        soma = soma + i
+    return soma
+    
 
 def conta_pares(lista):
-    """Devolve quantos numeros da lista sao pares."""
-    pass
-
+    conta_pares = 0
+    for i in lista:
+        if (i % 2 == 0):
+            conta_pares = conta_pares + 1
+    return conta_pares
+    
 
 def maior_valor(lista):
-    """Devolve o maior numero da lista. A lista nao esta vazia."""
-    pass
+    maior = lista[0]
+    for i in range(len(lista)):
+        if maior < lista[i]:
+            maior = lista[i]
+    return maior        
+
+
+def busca_linear(lista, alvo) :
+    for i in range (len(lista)):
+        if lista [i] == alvo:
+            return i 
+    return -1
 
 
 def existe(lista, alvo):
-    """Devolve True se o alvo esta na lista, False se nao esta."""
-    pass
-
+    for i in lista:
+        if i == alvo:
+            return True   
+    return False
 
 def busca_linear(lista, alvo):
-    """Devolve a posicao do alvo na lista, ou -1 se ele nao estiver."""
+    for i in range(len(lista)):
+        if lista[i] == alvo:
+            return i
+    return -1
     pass
 
 
 def segundo_maior(lista):
-    """(Desafio) Devolve o segundo maior, percorrendo a lista uma unica vez."""
-    pass
+    maior = lista[0]
+    segundo_maior = lista[1]
+    if maior < segundo_maior:
+        temp = maior
+        maior = segundo_maior
+        segundo_maior = temp
+    for n in lista[2:]:
+        if n > maior:
+            segundo_maior = maiormaior = n
+        elif n > segundo_maior:
+            segundo_maior = n
+    return segundo_maior
+    
